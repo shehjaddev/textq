cd1d80b259376a3bdb05c3639e1725cab14c7ef7
shehjaddev <shehjadcse@gmail.com>
2024-11-08 21:55:28 +0600

    fix: fix the issue of not capitalizing if a word start with " or '

--- lib/tools/converters.ts
+++ lib/tools/converters.ts
@@ -6,16 +6,27 @@ export const makeCapitalizeWord = (text: string) =>
   text
     .toLowerCase()
     .split(" ")
-    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+    .map((word) => {
+      if (word.startsWith(`"`) || word.startsWith(`'`)) {
+        return `${word.charAt(0)}${word.charAt(1).toUpperCase()}${word.slice(
+          2
+        )}`;
+      }
+      return word.charAt(0).toUpperCase() + word.slice(1);
+    })
     .join(" ");
 
 export const makeCapitalizeSentence = (text: string) =>
   text
     .toLowerCase()
     .split(/(\. |\n)/)
-    .map((sentence, index, arr) =>
-      index % 2 === 0
-        ? sentence.charAt(0).toUpperCase() + sentence.slice(1)
-        : sentence
-    )
+    .map((sentence, index) => {
+      if (index % 2 === 0) {
+        return sentence.replace(
+          /(^["'“‘]*)(\w)/,
+          (match, p1, p2) => p1 + p2.toUpperCase()
+        );
+      }
+      return sentence;
+    })
     .join("");
